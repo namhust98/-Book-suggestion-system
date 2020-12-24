f50b745d4f1ec0c9d129ff37a6bab13421edff85
namhust98 <namhust98@gmail.com>
2020-12-24 17:21:48 +0700

    fix config

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ BOOKS_FILE = './Data/books.csv'
 RATINGS_FILE = './Data/ratings.csv'
 
 # LSH parameters
-BLOCK_SIZE = 3  # ~ r
-NUM_BLOCKS = 500 # ~ p
-NUM_HASH_FUNCTIONS = 1500 # ~ K = r*p
+BLOCK_SIZE = 2  # ~ r
+NUM_BLOCKS = 1000 # ~ p
+NUM_HASH_FUNCTIONS = 2000 # ~ K = r*p
 NUM_NEAREST_NEIGHBORS = 5 # ~ nn
 nextPrime = 53437
 maxID = 53423
